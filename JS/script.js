c02e9abfbfa828a1fb02dedff93e9388004a6574
Nazik <aset_76@mail.ru>
2020-05-28 18:27:28 +0600

    Praktica2

--- JS/script.js
+++ JS/script.js
@@ -4,22 +4,46 @@
 let numberOfFilms = prompt('Сколько фильмов Вы просмотрели?', ' ');
 
 
+let a;
+let b;
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-    actors: {} ,
+    actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов', ' ');
-const b = prompt('Hа сколько оцените его', ' ');
-const c = prompt('Один из просмотренных фильмов', ' ');
-const d = prompt('Hа сколько оцените его', ' ');
 
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+for (let i = 0; i < 2; i++) {
+    a=prompt("Введите название фильма");
+    b=prompt("Оцените фильм");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+
+if (personalMovieDB.count < 10) {
+
+    alert("Вы посмотрели мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert('произошла ошибка');
+}
+// const a = prompt('Один из просмотренных фильмов', ' ');
+// const b = prompt('Hа сколько оцените его', ' ');
+// const c = prompt('Один из просмотренных фильмов', ' ');
+// const d = prompt('Hа сколько оцените его', ' ');
+
+
+//personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
