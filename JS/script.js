9129cf2f55d976dc84987bde0bd870d86ea016e4
Nazik <aset_76@mail.ru>
2020-05-28 14:57:33 +0600

    Praktika1

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,25 @@
-console.log(1);
+"use strict";
+
+
+let numberOfFilms = prompt('Сколько фильмов Вы просмотрели?', ' ');
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {} ,
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов', ' ');
+const b = prompt('Hа сколько оцените его', ' ');
+const c = prompt('Один из просмотренных фильмов', ' ');
+const d = prompt('Hа сколько оцените его', ' ');
+
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
